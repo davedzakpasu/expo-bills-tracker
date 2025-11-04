8fbb7a60088b51d21f7c12cb7193d991ada9b2a5
davedzakpasu <david.dzakpasu7@gmail.com>
2025-11-03 21:09:36 -0500

    Refactor code structure for improved readability and maintainability

--- app.config.js
+++ app.config.js
@@ -2,15 +2,26 @@ export default {
   expo: {
     name: "expo-bills-tracker",
     slug: "expo-bills-tracker",
-    platforms: ["ios", "android", "web"],
+    platforms: ["ios", "android"],
     web: {
       bundler: "metro",
       output: "single",
-      ssr: false, // force disable SSR (no expo-router)
+      ssr: false,
       favicon: "./assets/favicon.png",
+    },
+    ios: {
+      bundleIdentifier: "com.davedzakpasu.billstracker",
+    },
+    android: {
+      package: "com.davedzakpasu.billstracker",
     },
     experiments: {
       typedRoutes: false, // force disable expo-router auto detection
+    },
+    extra: {
+      eas: {
+        projectId: "2250156d-8029-4879-b60f-a3c0ccbe66b2",
+      },
     },
   },
 };
